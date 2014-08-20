3797f85dec64702cfad3e7e399f294ad70989b8b
haled <darren.e.hale@gmail.com>
2014-08-20 08:51:49 -0500

    Created a small test data set and figured out the basic UI interaction.

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,32 @@
 // hold javascript code
+var notebook = {
+  title: 'My Notes Thing',
+  located: 'Unknown',
+  notes: [
+    {
+      title:  'Todo List',
+      text:  'This is supposed to be a to-do list but I have not come close to implementing anything like that.',
+      createDateTime:  '8/18/2014',
+      modifyDateTime:  '8/18/2014'
+    },
+    {
+      title:  'Did this Work',
+      text:  'This is supposed to be a to-do list but I have not come close to implementing anything like that.',
+      createDateTime:  '8/18/2014',
+      modifyDateTime:  '8/18/2014'
+    }
+  ]
+};
+
+
+
+function getListOfNotes(currentNotebook) {
+  var list = '';
+  for (i = 0; i < 2; i++) {
+    list += "<li>" + currentNotebook.notes[i].title + "</li>";
+  }
+  return list;
+}
+
+document.getElementById("notes-list").innerHTML = getListOfNotes(notebook);
+
