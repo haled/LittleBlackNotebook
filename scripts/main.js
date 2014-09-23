7d2d4ca0800f4683de0e615a54b01ac3d188a665
haled <darren.e.hale@gmail.com>
2014-09-23 07:47:32 -0500

    Started to add the ability to create a new note.

--- scripts/main.js
+++ scripts/main.js
@@ -25,17 +25,37 @@ var notebook = {
 
 function getListOfNotes(currentNotebook) {
   var list = '';
-  for (i = 0; i < 2; i++) {
+  for (i = 0; i < currentNotebook.notes.length; i++) {
     list += "<li><a onclick=loadSelectedNote(" + i + ")>" + currentNotebook.notes[i].title + "</a></li>";
   }
   return list;
 }
 
+function createNewNote() {
+  var note = {
+    id: 3,
+    title: "Untitled",
+    text: "Type note here.",
+    createDateTime: "9/21/2014",
+    modifyDateTime: "9/21/2014"
+  };
+
+  notebook.notes[notebook.notes.length] = note;
+  alert("length -> " + notebook.notes.length);
+  $("#notes-list").html(getListOfNotes(notebook));
+
+}
+
 function loadSelectedNote(id) {
+  $("#note-title").val(notebook.notes[id].title);
   $("#note-area").val(notebook.notes[id].text);
   $("#btnSave").click(function() {
+      alert("saving index -> " + id);
       var noteText = $("#note-area").val();
+      var noteTitle = $("#note-title").val();
       notebook.notes[id].text = noteText;
+      notebook.notes[id].title = noteTitle;
+      $("#notes-list").html(getListOfNotes(notebook));
   });
 }
 
